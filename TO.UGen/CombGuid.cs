17431bb55ef43588763016ce86be972757f6bba0
Oleg Temnov <otemnov@academixdirect.com>
2020-10-29 19:22:25 +0300

    Introduced static CombGuid.CreateNew

---
+++ TO.UGen/CombGuid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TO.UGen
+{
+	public static class CombGuid
+	{
+		public static readonly ICombGuidGenerator Sql = new SqlCombGuidGenerator();
+
+		public static Guid CreateNew()
+		{
+			return Sql.CreateNew();
+		}
+	}
+}
